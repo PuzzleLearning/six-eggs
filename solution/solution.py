c1e174d77956976b196dda4fc9023dc64b5d87a1
ojarczyk <ojarczyk@mojorank.com>
2023-03-26 14:14:35 +0200

    writing the rationale in code

--- solution/solution.py
+++ solution/solution.py
@@ -1,2 +1,15 @@
+
+
 if __name__ == '__main__':
     print('Start.')
+    no_of_eggs = 6
+
+    eggs_broken = 2
+    eggs_unbroken = no_of_eggs-eggs_broken
+    print('We broke 2 eggs.')
+
+    print('We prepared 2 eggs.')
+    print('Note: This is vague, as we could "prepare" either the broken eggs, '
+          'unbroken eggs, or 1 broken and 1 unbroken. ')
+    print(' - preparing in this context simply mean applying some heat, "cooking them"')
+
